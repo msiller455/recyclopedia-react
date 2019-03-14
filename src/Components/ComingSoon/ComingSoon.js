abb7b96c432d49b9c225ae07ef53e5e09d86b6fd
Michael <msiller455@gmail.com>
2019-03-14 06:35:05 -0700

    adds comingSoon page

---
+++ src/Components/ComingSoon/ComingSoon.js
@@ -0,0 +1,11 @@
+import React from 'React'
+import './ComingSoon.css'
+
+const ComingSoon = () => {
+    return (
+        <div className="comingSoon">
+            <h1>Coming Soon</h1>
+        </div>
+
+    )
+}
